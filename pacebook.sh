f122c050b4e39a3836802327e62e176dded7e1c2
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-25 13:17:02 +0700

    Add files via upload

---
+++ pacebook.sh
@@ -0,0 +1,433 @@
+#!/system/xbin/bash
+#mau ngapain kontol mau ngerecode
+#ga akan jadiin lo mastah mikir dong
+#pake otak punya otak nggak lo kontol?
+#pliss don't reedite
+clear
+figlet LOGIN | lolcat
+echo
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "■■■■■■■■LOGIN DULU BROOOOO■■■■■■■■■■■■" | lolcat
+echo "■■■PILIH KODE UNTIK LANJUT KE TOOLS■■■" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo
+echo "1214 " | lolcat
+echo "8272 "
+echo "7266 " |lolcat
+echo "2727 " | lolcat
+echo "6262 "
+echo "2625 " | lolcat
+echo "1651 "
+echo "5555 " | lolcat
+echo "2000 " | lolcat
+echo "1765 "
+echo "2003 " | lolcat
+echo "9018 "
+echo "1927 " | lolcat
+echo "2004 " 
+echo "8265 " | lolcat
+echo "2019 "
+echo "2862 " | lolcat
+echo "8262 " | lolcat
+echo "2865 "
+echo "1982 "
+echo
+echo "kalau ga tau kodenya pm gue aja bro" | lolcat
+echo "Silahkan Dipilih" | lolcat
+echo
+read -p "BIGBOS STARLA~#" cok
+
+if [ $cok = 1214 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v"
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 8272 ]
+then
+clear
+toilet GAGAL  | lolcat
+echo "KODE SALAH COKK" | lolcat
+sleep 1
+echo "PM GUE AJA KALAU"
+echo "GAK TAU ITU COK!" | lolcat
+sleep 1
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 7266 ]
+then
+clear
+figlet GAGAL | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2727 ]
+then
+clear
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "MAAF NGEGAS :V"
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 6262 ]
+then
+clear
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "MAAF NGEGAS :V" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2625 ]
+then
+clear
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "MAAF NGEGAS :V" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 1651 ]
+then
+clear
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "MAAF NGEGAS :V" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 5555 ]
+then
+clear
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "MAAF NGEGAS :V" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2000 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v"
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 1765 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2003 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 9018 ]
+then
+clear
+figlet BERHASIL | lolcat
+sleep 3
+pkg install python2
+pkg install git
+pkg install figlet
+pkg install unzip
+pip2 install mechanize
+git clone https://github.com/yordankeres123/autoreport
+cd autoreport
+sh report.sh
+fi
+
+if [ $cok = 1927 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2004 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 8265 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2019 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2862 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi"
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v"
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 8262 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 2865 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah" | lolcat
+sleep 1
+echo "PM Gue kalau Gatau"
+sleep 1
+echo "Fb gue Bigbos Starla" | lolcat
+sleep 1
+echo "Nunggu lama Kan"
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!"
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+if [ $cok = 1982 ]
+then
+clear
+figlet GaGal | lolcat
+echo "Pasowrd salah"
+sleep 1
+echo "PM Gue kalau Gatau" | lolcat
+sleep 1
+echo "Fb gue Bigbos Starla"
+sleep 1
+echo "Nunggu lama Kan" | lolcat
+sleep 1
+echo "Tunggu 20 detik lagi" | lolcat
+sleep 20
+echo "PM gue aja Cokk!!!" | lolcat
+echo "Maaf Ngegas gue :v" | lolcat
+cd pacebook
+sh pacebook.sh
+fi
+
+
